7101f193570b92e35b879af18c3734e84559d404
prnav420 <118669489+prnav420@users.noreply.github.com>
2023-04-27 23:18:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // your code here
+document.addEventListener("DOMContentLoaded", function() {
+var name = document.getElementById("name");
+var year = document.getElementById("year");
+var h3 = document.getElementById("h3");
+var btn = document.getElementById("btn");
+
+var form = document.querySelector("form");
+
+function res(event) {
+  event.preventDefault();
+  var namecontent = name.value;
+  var yearcontent = year.value;
+  var h3cont = "https://localhost:8080/";
+
+  if (namecontent && yearcontent) {
+    h3cont += "?name=" + namecontent + "&year=" + yearcontent;
+  } else if (namecontent && !yearcontent) {
+    h3cont += "?name=" + namecontent;
+  } else if (!namecontent && yearcontent) {
+    h3cont += "?year=" + yearcontent;
+  } else {
+    h3cont += "";
+  }
+  h3.textContent = h3cont;
+}
+
+form.addEventListener("submit", res);
+});
